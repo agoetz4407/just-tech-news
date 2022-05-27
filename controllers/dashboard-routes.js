264422c96ed71585aecbd66870a11817ddac6b72
Andrew Goetz <agoetz4407@gmail.com>
2022-05-27 04:04:18 -0500

    everything working correctly except deleting posts - comment foreign key issue

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -42,6 +42,54 @@ router.get('/', checkAuth, (req, res) => {
     .catch(err => {
         console.log(err)
         res.status(500).json(err)
-    })})
+    })
+})
+
+router.get('/edit/:id', checkAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id', 'post_url', 'title', 'created_at',
+            [
+                sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                'vote_count'
+            ]
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                order: [['created_at', 'DESC']],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({message: 'No post found with given id'});
+            return;
+        }
+
+        const post = dbPostData.get({plain: true});
+
+        res.render('edit-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
 
 module.exports = router;
